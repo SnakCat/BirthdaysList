69fa50796c02923508065717350c8015b8896a6b
SnakCat <snakkdss@gmail.com>
2024-02-02 10:24:12 +0300

    initial commit

---
+++ BirthdaysList/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BirthdaysList
+//
+//  Created by Дмитрий Трушин on 02.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+    }
+    
+
+
+}
+
